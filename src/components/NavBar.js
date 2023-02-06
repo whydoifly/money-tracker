7cb041ba81494dad74c5a90880b9835d20807e0f
whydoifly <netpreecher@gmail.com>
2023-02-06 19:49:17 -0300

    Add some conditional showings

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,38 @@
 import { Link } from 'react-router-dom';
 import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 import styles from './NavBar.module.css';
 
 export default function NavBar() {
   const { logout } = useLogout();
+  const { user } = useAuthContext();
   return (
     <nav className={styles.navbar}>
       <ul>
         <li className={styles.title}>Money Track</li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Register</Link>
-        </li>
-        <li>
-          <button className='btn' onClick={logout}>
-            Logout
-          </button>
-        </li>
+
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Register</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+            <li>Hello, {user.displayName}</li>
+            <li>
+              <button className='btn' onClick={logout}>
+                Logout
+              </button>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
---
+++ src/hooks/useLogin.js
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react';
+import { projectAuth } from '../components/firebase/config';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogin = () => {
+  const [isCanceled, setIsCanceled] = useState(false);
+  const [error, setError] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const { dispatch } = useAuthContext();
+
+  const login = async (email, password) => {
+    setError(null);
+    setIsPending(true);
+
+    // sign the user out
+    try {
+      const res = await projectAuth.signInWithEmailAndPassword(email, password);
+
+      // dispatch logout action
+      dispatch({ type: 'LOGIN', payload: res.user });
+
+      // update state
+      if (!isCanceled) {
+        setIsPending(false);
+        setError(null);
+      }
+    } catch (error) {
+      if (!isCanceled) {
+        console.log(error.message);
+        setError(error.message);
+        setIsPending(false);
+      }
+    }
+  };
+
+  useEffect(() => {
+    return () => setIsCanceled(true);
+  }, []);
+
+  return { login, error, isPending };
+};
